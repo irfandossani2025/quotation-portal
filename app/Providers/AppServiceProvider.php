6fa02adf6f8c55f0581d7503f07b701048fac26c
irfandossani2025 <idossani.canada@gmail.com>
2026-09-05 15:27:44 +0400

    Support MariaDB 10.1 index limits

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // MariaDB 10.1 supports a maximum indexed key length of 767 bytes.
+        Schema::defaultStringLength(191);
     }
 }
